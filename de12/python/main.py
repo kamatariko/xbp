e1a82df1c02f21c198ec86b04cf484920f91ce97
kamatariko <r202202229gz@jindai.jp>
2022-10-27 14:07:48 +0900

    1027

---
+++ de12/python/main.py
@@ -0,0 +1,11 @@
+from operator import length_hint
+
+
+name="aaa"
+waist=72
+age=27
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
+
+#baseにしてから▶マークを押す
+
---
+++ de12/python/work2.py
@@ -0,0 +1,4 @@
+name=input("名前を教えて下さい")
+waist=input("腹囲は？")
+age=input("年齢は？")
+
